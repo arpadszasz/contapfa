d2ad11fa00738cb96ef64534db67c69d9642f3be
Árpád Szász <arpad.szasz@plenum.ro>
2014-01-08 16:06:10 +0200

    Basic SQL schema script

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+BEGIN TRANSACTION;
+
+PRAGMA foreign_keys = ON;
+PRAGMA encoding = "UTF-8";
+
+CREATE TABLE metadata (
+    id          INTEGER PRIMARY KEY ASC,
+    version     VARCHAR(16),
+    release     TIMESTAMP NOT NULL
+);
+
+INSERT INTO metadata (id, version, release) VALUES ("1.0", datetime('now', 'localtime'));
+
+COMMIT;
+
